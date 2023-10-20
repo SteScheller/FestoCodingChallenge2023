1b8abe9b6ae8070603e928efdc4e8ec91675f62b
Stefan Scheller <stefan.scheller@endress.com>
2023-10-20 15:08:32 +0200

    started puzzle 3 chapter 3

---
+++ chapter3/puzzle3.py
@@ -0,0 +1,24 @@
+from typing import Tuple, Optional
+from re import search
+
+Flasks = Tuple[int]
+
+
+def get_fixed_flasks(input_: str) -> Flasks:
+    return tuple(int(x) for x in input_.split(" ") if x != "X")
+
+
+def get_free_flasks(input_: str) -> Flasks:
+    return tuple(int(x[1:-1]) for x in input_.split(" ") if x != "X")
+
+
+def parse_config(config: str) -> Tuple[Optional[int], Flasks]:
+    m = search(r"(\d+: )?([\d ]+) - ((\(\d+\) ?)+)", config)
+    id_, l, r, _ = m.groups()
+    if id_ is not None:
+        id_ = int(id_[:-2])
+    return id_, get_fixed_flasks(l), get_free_flasks(r)
+
+
+def can_be_balanced(left: Flasks, right: Flasks) -> bool:
+    return False
---
+++ chapter3/test_puzzle3.py
@@ -0,0 +1,34 @@
+from typing import Optional
+
+import pytest
+
+import chapter3.puzzle3 as puzzle3
+
+
+@pytest.mark.parametrize(
+    "config, id_, fixed_flasks, free_flasks",
+    [
+        ("2 - (6)", None, (2,), (6,)),
+        ("6 - (2) (3)", None, (6,), (2, 3)),
+        ("  60: 2 9 396 - (29) (46) (52)", 60, (2, 9, 396), (29, 46, 52)),
+        ("  61: 3 24 - (14) (17)", 61, (3, 24), (14, 17)),
+        ("  62: 5 25 850 - (35) (97) (1802)", 62, (5, 25, 850), (35, 97, 1802)),
+        ("  63: 4 27 2160 - (356) (632) (1424)", 63, (4, 27, 2160), (356, 632, 1424)),
+    ],
+)
+def test_parse_config(
+    config: str, id_: Optional[bool], fixed_flasks: puzzle3.Flasks, free_flasks: puzzle3.Flasks
+):
+    assert puzzle3.parse_config(config) == (id_, fixed_flasks, free_flasks)
+
+
+@pytest.mark.parametrize(
+    "config, result",
+    [
+        ("2 - (6)", True),
+        ("6 - (2) (3)", True),
+    ],
+)
+def test_can_be_balanced(config: str, result: bool):
+    _, flasks, _ = puzzle3.parse_config(config)
+    assert puzzle3.can_be_balanced(flasks, tuple()) == result
