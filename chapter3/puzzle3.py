142a675040d144a4fb8416896025683d284182a1
Stefan Scheller <stefan.scheller@endress.com>
2023-10-25 10:36:27 +0200

    worked on chapter 3 puzzle 3

--- chapter3/puzzle3.py
+++ chapter3/puzzle3.py
@@ -1,7 +1,60 @@
-from typing import Tuple, Optional
+from typing import Tuple, Optional, Set
 from re import search
+from math import gcd
+from operator import mul
+from itertools import permutations
+from functools import total_ordering, reduce, cache
+from copy import deepcopy
 
 Flasks = Tuple[int]
+
+
+@total_ordering
+class Fraction:
+    def __init__(self, nom, denom):
+        c = gcd(nom, denom)
+        self.__nom = nom // c
+        self.__denom = denom // c
+
+    @property
+    def nom(self):
+        return self.__nom
+
+    @nom.setter
+    def nom(self, value):
+        c = gcd(value, self.__denom)
+        self.__nom = value // c
+        self.__denom = self.__denom // c
+
+    @property
+    def denom(self):
+        return self.__denom
+
+    @denom.setter
+    def denom(self, value):
+        c = gcd(value, self.__nom)
+        self.__nom = self.__nom // c
+        self.__denom = value // c
+
+    def __add__(self, other):
+        n = self.__nom * other.denom + other.nom * self.__denom
+        d = self.__denom * other.denom
+        return Fraction(n, d)
+
+    def __sub__(self, other):
+        n = self.__nom * other.denom - other.nom * self.__denom
+        d = self.__denom * other.denom
+        return Fraction(n, d)
+
+    def __eq__(self, other):
+        return (self.__nom == other.nom) and (self.__denom == other.denom)
+
+    def __lt__(self, other):
+        t = self - other
+        return (t.nom * t.denom) <= 0
+
+    def __hash__(self):
+        return hash((self.__nom, self.__denom))
 
 
 def get_fixed_flasks(input_: str) -> Flasks:
@@ -20,5 +73,39 @@ def parse_config(config: str) -> Tuple[Optional[int], Flasks]:
     return id_, get_fixed_flasks(l), get_free_flasks(r)
 
 
-def can_be_balanced(left: Flasks, right: Flasks) -> bool:
-    return False
+def compute_water_fractions(flasks: Flasks) -> Set[Fraction]:
+    fractions = {Fraction(1, n) for n in flasks}
+
+    for x, y in permutations(fractions, 2):
+        temp = deepcopy(y)
+        while temp <= x:
+            fractions.add(temp)
+            temp += y
+        fractions.add(temp - x)
+
+    return fractions
+
+
+@cache
+def compute_weight_fraction(flasks: Flasks) -> Fraction:
+    if len(flasks) == 0:
+        nom = 0
+        denom = 0
+    elif len(flasks) == 1:
+        nom = 1
+        denom = flasks[0]
+    else:
+        summands = list()
+        for i in range(len(flasks)):
+            x, y = flasks[:i], flasks[i + 1 :]
+            factor = reduce(mul, x + y)
+            summands.append(factor)
+        nom = sum(summands)
+        denom = reduce(mul, flasks)
+    return Fraction(nom, denom)
+
+
+def can_be_balanced(fixed: Flasks, free: Flasks) -> bool:
+    target = compute_weight_fraction(fixed)
+    water_fractions = compute_water_fractions(free)
+    return target in water_fractions
--- chapter3/test_puzzle3.py
+++ chapter3/test_puzzle3.py
@@ -1,8 +1,15 @@
-from typing import Optional
+from typing import Optional, List, Tuple
 
 import pytest
 
-import chapter3.puzzle3 as puzzle3
+from chapter3.puzzle3 import (
+    Flasks,
+    Fraction,
+    parse_config,
+    compute_water_fractions,
+    compute_weight_fraction,
+    can_be_balanced,
+)
 
 
 @pytest.mark.parametrize(
@@ -16,19 +23,40 @@ import chapter3.puzzle3 as puzzle3
         ("  63: 4 27 2160 - (356) (632) (1424)", 63, (4, 27, 2160), (356, 632, 1424)),
     ],
 )
-def test_parse_config(
-    config: str, id_: Optional[bool], fixed_flasks: puzzle3.Flasks, free_flasks: puzzle3.Flasks
-):
-    assert puzzle3.parse_config(config) == (id_, fixed_flasks, free_flasks)
+def test_parse_config(config: str, id_: Optional[bool], fixed_flasks: Flasks, free_flasks: Flasks):
+    assert parse_config(config) == (id_, fixed_flasks, free_flasks)
 
 
-# @pytest.mark.parametrize(
-#     "config, result",
-#     [
-#         ("2 - (6)", True),
-#         ("6 - (2) (3)", True),
-#     ],
-# )
-# def test_can_be_balanced(config: str, result: bool):
-#     _, flasks, _ = puzzle3.parse_config(config)
-#     assert puzzle3.can_be_balanced(flasks, tuple()) == result
+@pytest.mark.parametrize(
+    "flasks, fraction_values",
+    [
+        ((2, 3), {(1, 2), (1, 3), (1, 6)}),
+    ],
+)
+def test_compute_water_fractions(flasks: Flasks, fraction_values: List[Tuple[int, int]]):
+    fractions = {Fraction(n, d) for (n, d) in fraction_values}
+    assert compute_water_fractions(flasks) == fractions
+
+
+@pytest.mark.parametrize(
+    "flasks, fraction_values",
+    [
+        ((2, 3), (5, 6)),
+        ((1, 2, 3), (11, 6)),
+        ((2, 1000000), (500001, 1000000)),
+    ],
+)
+def test_compute_weight_fraction(flasks: Flasks, fraction_values: List[Tuple[int, int]]):
+    assert compute_weight_fraction(flasks) == Fraction(*fraction_values)
+
+
+@pytest.mark.parametrize(
+    "config, result",
+    [
+        ("2 - (6)", True),
+        ("6 - (2) (3)", True),
+    ],
+)
+def test_can_be_balanced(config: str, result: bool):
+    _, flasks, _ = parse_config(config)
+    assert can_be_balanced(flasks, tuple()) == result
